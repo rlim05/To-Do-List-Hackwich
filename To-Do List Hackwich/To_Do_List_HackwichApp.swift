3c4eca295862bc9c283fd1f5e8eee56902d75fbf
Ryan Lim <rlim@bsd220.org>
2022-01-10 10:32:48 -0600

    Initial Commit

---
+++ To-Do List Hackwich/To_Do_List_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_HackwichApp.swift
+//  To-Do List Hackwich
+//
+//  Created by Student on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
