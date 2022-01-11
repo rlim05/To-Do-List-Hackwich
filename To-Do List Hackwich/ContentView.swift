0688eea55e3326afbc695563b358cea84e22be44
Ryan Lim <rlim@bsd220.org>
2022-01-10 20:30:38 -0600

    Enabled Data Persistence

--- To-Do List Hackwich/ContentView.swift
+++ To-Do List Hackwich/ContentView.swift
@@ -47,7 +47,7 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct ToDoItem: Identifiable {
+struct ToDoItem: Identifiable, Codable {
     var id = UUID()
     var priority = String()
     var description = String()
