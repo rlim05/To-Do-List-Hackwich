1c635af92c46e73360ef9d8daa6c88e2d2823026
Ryan Lim <rlim@bsd220.org>
2022-01-10 20:13:16 -0600

    Created Add Items View

---
+++ To-Do List Hackwich/AddItemView.swift
@@ -0,0 +1,34 @@
+//
+//  AddItemView.swift
+//  To-Do List Hackwich
+//
+//  Created by Student on 1/10/22.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    @ObservedObject var toDoList: ToDoList
+    @State private var priority = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+    static let priorities = ["High", "Medium", "Low"]
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Priority", selection: $priority) {
+                    ForEach(Self.priorities, id: \.self) { priority in
+                        Text(priority)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AddItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddItemView(toDoList: ToDoList())
+    }
+}
